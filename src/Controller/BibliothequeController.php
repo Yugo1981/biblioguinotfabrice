d83c96ffe8323d600617c624682ab26ca743b4df
Ffollereau <foller.fabrice@gmail.com>
2021-10-19 14:24:19 +0200

    Changement de home

--- src/Controller/BibliothequeController.php
+++ src/Controller/BibliothequeController.php
@@ -6,9 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/bibliotheque")
- */
+
 
 class BibliothequeController extends AbstractController
 {
